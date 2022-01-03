a257e3fb178afde75a1590a102213a8abe006d74
gwansik <rhkstlr00@gmail.com>
2022-01-03 17:34:52 +0900

    fetch 사용해 데이터 불러오기

---
+++ project_01/node/index.js
@@ -0,0 +1,36 @@
+// npm init --yes
+// npm i express cors
+// npm install --save-dev nodemon 
+// package.json에서 
+// "test": "echo \"Error: no test specified\" && exit 1"를
+// "start" : "nodemon index.js"로 고치고
+// npm start
+
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const postRoute = require("./routes/posts");
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/posts", postRoute);
+
+// 페이지 불러오기
+app.get('/', function(req, res, next) {
+  res.send('index');
+});
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+})
+  
+app.use((err, req, res, next) => {
+  console.log('애러났음!')
+  console.log(err);
+  res.sendStatus(500);
+})
+
+app.listen("8080", () => {
+  console.log("노드 실행 중");
+});
